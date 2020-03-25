24772ce5e24d0c4ed9ec265f6cceef8e585c91cb
Samuel Terra <samuelterra22@hotmail.com>
2020-03-25 15:05:51 -0300

    Fix create ongs

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -8,7 +8,7 @@ module.exports = {
     return response.json(ongs)
   },
 
-  async create (response, request) {
+  async create (request, response) {
     const {name, email, whatsapp, city, uf} = request.body
 
     const id = crypto.randomBytes(4).toString('HEX')
